2a00b2e4c43bef1f02ad4d9bfd8a52c17471cec9
Ani-codes <84683871+Ani-codes@users.noreply.github.com>
2021-06-28 05:35:19 +0530

    Update buttons.py

--- library/buttons.py
+++ library/buttons.py
@@ -4,36 +4,36 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 #Buttons used
 
 home_button = [
-    [InlineKeyboardButton("🌏 Source", "source_btn"),
-     InlineKeyboardButton("⬇️  Fr. Id", "from_btn"),
-     InlineKeyboardButton("❓ Help", url="https://telegra.ph/Clonebot-UI-Help-05-30")],
-    [InlineKeyboardButton("🎯 Target", "target_btn"),
-     InlineKeyboardButton("⬆️  To Id", "up_to_btn"),
-     InlineKeyboardButton("Type  ➡", "types_btn")],
+    [InlineKeyboardButton("source", "source_btn"),
+     InlineKeyboardButton("Fr. Id", "from_btn"),
+     InlineKeyboardButton("Help", url="https://telegra.ph/Clonebot-UI-Help-05-30")],
+    [InlineKeyboardButton("Target", "target_btn"),
+     InlineKeyboardButton("To Id", "up_to_btn"),
+     InlineKeyboardButton("Type ", "types_btn")],
     [InlineKeyboardButton("Delayed", "delay_btn"),
      InlineKeyboardButton("Caption", "caption_btn"),
      InlineKeyboardButton("FNAC", "f_caption_btn")],
-    [InlineKeyboardButton("🔎️  View", "view_btn"),
-     InlineKeyboardButton("🚮  Reset", "rst_btn"),
-     InlineKeyboardButton("❌  Close", "close_btn")],
-    [InlineKeyboardButton("🚦 Clone Medias 🚦", "clone_btn")]
+    [InlineKeyboardButton("View", "view_btn"),
+     InlineKeyboardButton("Reset", "rst_btn"),
+     InlineKeyboardButton("Close", "close_btn")],
+    [InlineKeyboardButton("📂 Clone Medias 📂", "clone_btn")]
 ]
 
 
 start_button = [
-    [InlineKeyboardButton("🏅 GitHub 🏅", url="github.com/m4mallu/clonebot-ui"),
-     InlineKeyboardButton("⚙️Settings ⚙", "start_btn")]
+    [InlineKeyboardButton("GitHub", url="github.com/m4mallu/clonebot-ui"),
+     InlineKeyboardButton("Settings", "start_btn")]
 ]
 
 
 types_button = [
-    [InlineKeyboardButton(" ⏺ Docs", "docs_btn"),
-     InlineKeyboardButton(" ⏺ Video", "video_btn"),
-     InlineKeyboardButton(" ⏺ Audio", "audio_btn")],
-    [InlineKeyboardButton(" ⏺ Photo", "photo_btn"),
-     InlineKeyboardButton(" ⏺ Voice", "voice_btn"),
-     InlineKeyboardButton("⚙️ View", "view_types")],
-    [InlineKeyboardButton("⬅️ Back", "start_btn")]
+    [InlineKeyboardButton(" Docs", "docs_btn"),
+     InlineKeyboardButton(" Video", "video_btn"),
+     InlineKeyboardButton(" Audio", "audio_btn")],
+    [InlineKeyboardButton(" Photo", "photo_btn"),
+     InlineKeyboardButton(" Voice", "voice_btn"),
+     InlineKeyboardButton(" View", "view_types")],
+    [InlineKeyboardButton(" Back", "start_btn")]
 ]
 
 
@@ -43,16 +43,16 @@ stop_button = [
 
 
 finished_button = [
-    [InlineKeyboardButton("🏠  HOME", "start_btn"),
-     InlineKeyboardButton("❌  CLOSE", "close_btn")]
+    [InlineKeyboardButton("HOME", "start_btn"),
+     InlineKeyboardButton("CLOSE", "close_btn")]
 ]
 
 
 terminate_btn = [
-    [InlineKeyboardButton("🧸 Updates", url="https://github.com/m4mallu/clonebot-ui"),
-     InlineKeyboardButton("❓ Usage", url="https://telegra.ph/Clonebot-UI-Help-05-30")],
-    [InlineKeyboardButton("🚫 Terminate", "terminate_btn"),
-     InlineKeyboardButton("🏠 Home", "start_btn")]
+    [InlineKeyboardButton("Updates", url="https://github.com/m4mallu/clonebot-ui"),
+     InlineKeyboardButton("Usage", url="https://telegra.ph/Clonebot-UI-Help-05-30")],
+    [InlineKeyboardButton("Terminate", "terminate_btn"),
+     InlineKeyboardButton("Home", "start_btn")]
 ]
 
 
